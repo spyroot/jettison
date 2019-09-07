3c6f4615842c0e5c7c99775ba8ca72f8e2d90ffe
spyroot <spyroot@gmail.com>
2019-09-08 02:53:41 +0400

    Initial commit

---
+++ testutils/testutils.go
@@ -0,0 +1,107 @@
+/*
+Copyright (c) 2015 VMware, Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Basic helpers for unit testing.
+
+Author Mustafa Bayramov
+mbaraymov@vmware.com
+*/
+
+package testutils
+
+import (
+	"context"
+	"github.com/spyroot/jettison/vcenter"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vmdk"
+	_ "github.com/vmware/govmomi/vmdk"
+	"log"
+)
+
+var (
+	// path to a file
+	TestImage string = "/Users/spyroot/go/tests/ttylinux-disk001.vmdk"
+	// cluster name
+	TestImageCluster string = "mgmt"
+	// data store attached to a cluster
+	TestDataStoreName string = "vsanMgmtDatastore"
+	// name of vm
+	TestImageName = "jettison-test"
+)
+
+/**
+  Function create test vm if vm not present and return VM uuid.
+*/
+func CreateVmifneeded(ctx context.Context, c *vim25.Client) (string, error) {
+
+	_, _, vm, err := vcenter.VmFromCluster(ctx, c, TestImageName, TestImageCluster)
+	switch t := err.(type) {
+	case *vcenter.VmNotFound:
+		uplerr := UploadImage(ctx, c)
+		if uplerr != nil {
+			return "", uplerr
+		}
+		_, _, vm, err = vcenter.VmFromCluster(ctx, c, TestImageName, TestImageCluster)
+		if err != nil {
+			return "", err
+		}
+	case error:
+		return "", t
+	default:
+	}
+
+	return vm.UUID(ctx), nil
+}
+
+/*
+   Upload test image to vCenter.
+*/
+func UploadImage(ctx context.Context, c *vim25.Client) error {
+
+	ds, err := vcenter.FindDatastore(ctx, c, TestImageCluster, TestDataStoreName)
+	if err != nil {
+		return err
+	}
+
+	h := NewHelper(c)
+	df, err := h.Datacenter().Folders(ctx)
+	if err != nil {
+		return err
+	}
+
+	cr := h.ComputeResource(TestImageCluster)
+	dss, err := h.LocalDatastores(ctx, cr)
+	if err != nil {
+		return err
+	}
+
+	if len(dss) == 0 {
+		log.Fatalf("No local datastores")
+	}
+
+	// Get root resource pool for compute resource
+	rp, err := cr.ResourcePool(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var p = vmdk.ImportParams{}
+	p.Datacenter = h.Datacenter()
+	p.Folder = df.VmFolder
+	p.Pool = rp
+	p.Path = TestImageName
+
+	return Import(ctx, c, TestImage, ds, ImportParams(p))
+}
