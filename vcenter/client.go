75fd4636c22a41569fce4384dcdb8ccc1abc576d
spyroot <spyroot@gmail.com>
2019-09-29 00:05:45 +0400

    Initial commit

--- vcenter/client.go
+++ vcenter/client.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 VMware, Inc. All Rights Reserved.
+Copyright (c) 2019 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/spyroot/jettison/logging"
 	"github.com/spyroot/jettison/osutil"
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/vim25/soap"
@@ -43,12 +44,8 @@ const (
 var urlDescription = fmt.Sprintf("ESX or vCenter URL [%s]", envURL)
 var urlFlag = flag.String("url", osutil.GetEnvString(envURL, ""), urlDescription)
 
-//var insecureDescription = fmt.Sprintf("Don't verify the server's certificate chain [%s]", envInsecure)
-//var insecureFlag = flag.Bool("insecure", osutil.GetEnvBool(envInsecure, false), insecureDescription)
-
-/*
-  Function open up connection to vCenter or ESXi.
-*/
+//  Function open up connection to vCenter or ESXi.
+//
 func Connect(ctx context.Context, hostname string, username string, password string) (*govmomi.Client, error) {
 
 	flag.Parse()
@@ -71,7 +68,12 @@ func Connect(ctx context.Context, hostname string, username string, password str
 		u.User = url.UserPassword(username, password)
 	}
 
-	// Connect and log in to ESX or vCenter
+	client, err := govmomi.NewClient(ctx, u, true)
+	if err != nil {
+		logging.CriticalMessage("Failed to connect to vmware vim")
+		return nil, err
+	}
+
 	log.Print("Connected to vCenter manager and return context")
-	return govmomi.NewClient(ctx, u, true)
+	return client, nil
 }
